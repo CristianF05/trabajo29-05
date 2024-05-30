8fff0f75858f3671f0f00df670dd89f0ccc9bf52
CristianF05 <1482670@senati.pe>
2024-05-30 07:45:37 -0500

    terminado con myel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\tablaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,8 @@ Route::get('/vista', [EstudianteController::class, 'index']);
 
 // Ruta para procesar el formulario de registro de estudiantes (POST)
 Route::post('/guardar-estudiante', [EstudianteController::class, 'store'])->name('guardar.estudiante'); 
+
+// Rutas para la tabla de estudiantes (CRUD)
+Route::get('/tabla', [TablaController::class, 'index'])->name('estudiantes.index');
+Route::put('/tabla/{id}', [TablaController::class, 'update'])->name('estudiantes.update');
+Route::delete('/tabla/{id}', [TablaController::class, 'destroy'])->name('estudiantes.destroy');
